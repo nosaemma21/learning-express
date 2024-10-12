40bb6cc5946ab8d539e24a814ad9773fc34233b4
nosaemma21 <emmanuelnosakhare052@gmail.com>
2024-10-12 17:12:47 +0100

    made few changes

--- app.js
+++ app.js
@@ -1,21 +1,11 @@
 const express = require("express");
-const path = require("path");
 const app = express();
+const { people } = require("./data");
 
-app.use(express.static("./public"));
-
-// app.get("/", (req, res) => {
-//   res.sendFile(path.resolve(__dirname, "./navbar-app/index.html"));
-// });
-
-// adding to static assets
-
-// SSR (Server-Side Rendering)
-
-app.all("*", (req, res) => {
-  res.status(404).send("resource not found");
+app.get("/api/people", (req, res) => {
+  res.status(200).json({ success: true, data: [people] });
 });
 
 app.listen(5000, () => {
-  console.log("Server listening on port 5000");
+  console.log("Server is listening on port 5000...");
 });
---
+++ logger.js
@@ -0,0 +1,7 @@
+module.exports = function logger(req, res, next) {
+  const method = req.method;
+  const url = req.url;
+  const time = new Date().getFullYear();
+  console.log(method, url, time);
+  next();
+};
