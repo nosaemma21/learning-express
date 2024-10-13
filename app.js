32af5a46cf941a0b9fd3b18a402a6aabaa92f5f0
nosaemma21 <emmanuelnosakhare052@gmail.com>
2024-10-13 17:07:11 +0100

    made final changes

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
-const { people } = require("./data");
+const peopleRouter = require("./routes/people.js");
+const loginRouter = require("./routes/auth.js");
 
 // Static assets
 app.use(express.static("./methods-public"));
@@ -9,22 +10,9 @@ app.use(express.static("./methods-public"));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.get("/api/people", (req, res) => {
-  res.status(200).json({ success: true, data: people });
-});
-
-app.post("/login", (req, res) => {
-  const { name } = req.body;
-
-  if (name) {
-    return res.status(200).send(`Welcome ${name}`);
-  }
-  res.status(401).send("Please provide credentials");
-});
-
-app.post("/api/people", (req, res) => {
-  res.status(201).send("Success");
-});
+// Routers
+app.use("/api/people", peopleRouter);
+app.use("/login", loginRouter);
 
 app.listen(5000, () => {
   console.log("Server is listening on port 5000...");
